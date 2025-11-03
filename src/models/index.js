ea1b4128f5607e1208a3793372cdae2eae0d25f3
Nguyen Duc Khanh <khanhhd8880@gmail.com>
2025-11-03 17:40:54 +0700

    delete file index

--- src/models/index.js
+++
@@ -1,6 +0,0 @@
-module.exports.User = require('./user.model');
-module.exports.Category = require('./category.model');
-module.exports.Product = require('./product.model');
-module.exports.Order = require('./order.model');
-module.exports.Review = require('./review.model');
-module.exports.Cart = require('./cart.model');
--- src/services/index.js
+++
@@ -1,5 +0,0 @@
-module.exports.userService = require('./user.service');
-module.exports.authService = require('./auth.service');
-module.exports.categoryService = require('./category.service');
-module.exports.productService = require('./product.service');
-module.exports.orderService = require('./order.service');
