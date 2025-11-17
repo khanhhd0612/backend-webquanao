613a6682270e9deba48e75942ba3ef121090670f
Nguyen Duc Khanh <khanhhd8880@gmail.com>
2025-11-17 18:00:48 +0700

    docs: add api document by swagger

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const routes = require("./src/routes/v1");
 const { authLimiter } = require('./src/middlewares/rateLimiter');
 const config = require('./src/config/config');
 const ApiError = require('./src/utils/ApiError');
 const { errorConverter, errorHandler } = require('./src/middlewares/error');
 const passport = require('./src/config/passport');
-
+const { swaggerUi, swaggerSpec } = require('./src/config/swagger');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -16,6 +19,17 @@ app.use(passport.initialize());
 
 app.get("/", (req, res) => {
     res.send("Server is running!");
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+    explorer: true,
+    customCss: '.swagger-ui .topbar { display: none }',
+    customSiteTitle: "E-Commerce API Documentation"
+}));
+
+app.get('/api-docs.json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
 });
 
 //rate limit
