2173cb9fcc7128175349dd994837e498509ce921
Nguyen Duc Khanh <khanhhd8880@gmail.com>
2025-12-08 02:44:33 +0700

    feat: switch send jwt from client-side storage to HttpOnly cookies

---
+++ src/config/cors.js
@@ -0,0 +1,14 @@
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:5000'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+};
+
+module.exports = corsOptions;
