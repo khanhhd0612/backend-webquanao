322eb07e44bc0bc0c3a668cffab97d2a0a7c4a11
Nguyen Duc Khanh <khanhhd8880@gmail.com>
2025-12-08 02:51:15 +0700

    feat: add get user profile route and logout route

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -2,11 +2,18 @@ const express = require('express');
 const validate = require('../../middlewares/validate');
 const authValidation = require('../../validations/auth.validation');
 const authController = require('../../controllers/auth.controller');
+const auth = require('../../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/register', validate(authValidation.register), authController.register);
 
+router.get('/me', auth(), (req, res) => {
+    res.json({ user: req.user });
+})
+
 router.post('/login', validate(authValidation.login), authController.login);
+
+router.post('/logout', auth(), authController.logout);
 
 module.exports = router;
