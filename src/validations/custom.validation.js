56f6a01d9927af7c49ebd937bd8741ce6b83db84
nguyen duc khanh <khanhhd8880@gmail.com>
2025-12-23 16:54:34 +0700

    feat: change message to vietnamese

--- src/validations/custom.validation.js
+++ src/validations/custom.validation.js
@@ -1,16 +1,16 @@
 const objectId = (value, helpers) => {
     if (!value.match(/^[0-9a-fA-F]{24}$/)) {
-        return helpers.message('"{{#label}}" must be a valid mongo id');
+        return helpers.message('"{{#label}}" phải là một ID Mongo hợp lệ');
     }
     return value;
 };
 
 const password = (value, helpers) => {
     if (value.length < 8) {
-        return helpers.message('password must be at least 8 characters');
+        return helpers.message('Mật khẩu phải có ít nhất 8 ký tự');
     }
     if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-        return helpers.message('password must contain at least 1 letter and 1 number');
+        return helpers.message('Mật khẩu phải chứa ít nhất 1 chữ cái và 1 số');
     }
     return value;
 };
